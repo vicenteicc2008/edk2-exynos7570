e658dcfad7e3c63ce1125050e9a7c14ed35ea3cd
Vicente Cortes <vicenteicc2008@gmail.com>
2023-08-25 17:53:33 -0400

    add dtb to workspace

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ set -e
 ./build_bootshim.sh
 GCC5_AARCH64_PREFIX=aarch64-linux-gnu- build -j$(nproc) -s -n 0 -a AARCH64 -t GCC5 -p EXYNOS7885Pkg/Devices/j4lte.dsc
 cat BootShim/BootShim.bin workspace/Build/EXYNOS7885Pkg/DEBUG_GCC5/FV/EXYNOS7885PKG_UEFI.fd > workspace/UEFI
-mkbootimg --kernel workspace/UEFI --base 0x010000000 -o workspace/boot.img
+mkbootimg --kernel workspace/UEFI --base 0x010000000 --tags_offset 0x00000100 --pagesize 2048 --header_version 2 --dtb workspace/dtb -o workspace/boot.img
 cd workspace/
 tar -c boot.img -f boot.tar
 cd ..
