432b2c0161783e53f2380f1ac397d0eb3ba6248e
Vicente Cortes <vicenteicc2008@gmail.com>
2023-09-22 23:36:11 -0300

    broken again

--- EXYNOS7885Pkg/Drivers/GpioDxe/GpioDxe.c
+++ EXYNOS7885Pkg/Drivers/GpioDxe/GpioDxe.c
@@ -9,16 +9,6 @@ ExynosGpioEntryPoint (
     IN EFI_SYSTEM_TABLE  *SystemTable
 )
 {
-    EFI_STATUS Status;
-
-    // Inicializar la biblioteca UEFI para permitir la impresión en pantalla.
-    Status = InitializeLib(ImageHandle, SystemTable);
-    if (EFI_ERROR(Status)) {
-        return Status;
-    }
-
-    // Agregar aquí la lógica para interactuar con los pines GPIO del procesador Exynos.
-
     DEBUG((EFI_D_INFO, "Hello World\n"));
 
     // Devolver EFI_SUCCESS para indicar que el controlador se ha inicializado correctamente.
