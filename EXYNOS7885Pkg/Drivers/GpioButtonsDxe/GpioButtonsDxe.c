4757f87c65181cd34c65fe1b13dea54449dd4646
Vicente Cortes <vicenteicc2008@gmail.com>
2023-09-24 13:18:25 -0300

    update some code in gpiobuttonsdxe

--- EXYNOS7885Pkg/Drivers/GpioButtonsDxe/GpioButtonsDxe.c
+++ EXYNOS7885Pkg/Drivers/GpioButtonsDxe/GpioButtonsDxe.c
@@ -2,66 +2,117 @@
 #include <Library/UefiLib.h>
 #include <Library/UefiBootServicesTableLib.h>
 #include <Library/DebugLib.h>
-#include <Protocol/ExynosButtons.h>  // Supongamos que existe un protocolo personalizado para los botones.
+#include <Protocol/ExynosButtons.h>  // Supposing a custom protocol for buttons exists.
+#include <Protocol/Gpio.h>           // Include the GPIO protocol.
 
-#define VOLUME_UP_GPIO_PIN 7
+#define VOLUME_UP_GPIO_PIN   7
 #define VOLUME_DOWN_GPIO_PIN 3
 
 EFI_STATUS WaitForPowerKey() {
     EFI_STATUS Status;
-    BUTTON_STATE PowerKeyState;  // Cambia el tipo de datos a BUTTON_STATE
+    BUTTON_STATE PowerKeyState;  // Change the data type to BUTTON_STATE.
 
-    // Supongamos que tienes un protocolo personalizado ExynosButtonsProtocol
-    // que proporciona acceso a los botones físicos, incluido el botón de encendido ("power key").
+    // Supposing you have a custom ExynosButtonsProtocol that provides access to physical buttons, including the power key.
     EXYNOS_BUTTONS_PROTOCOL *ButtonsProtocol;
 
     Status = gBS->LocateProtocol(
-        &gExynosButtonsProtocolGuid,  // Reemplaza con la GUID de tu protocolo de botones.
+        &gExynosButtonsProtocolGuid,  // Replace with the GUID of your button protocol.
         NULL,
         (VOID**)&ButtonsProtocol
     );
 
     if (EFI_ERROR(Status)) {
-        DEBUG((EFI_D_ERROR, "No se pudo encontrar el protocolo de botones: %r\n", Status));
+        DEBUG((EFI_D_ERROR, "Failed to locate the button protocol: %r\n", Status));
         return Status;
     }
 
     do {
-        // Leer el estado del botón de encendido ("power key").
-        Status = ButtonsProtocol->ReadEnterKeyState(ButtonsProtocol, &PowerKeyState);  // Cambia el tipo de datos a BUTTON_STATE*
+        // Read the state of the power key.
+        Status = ButtonsProtocol->ReadEnterKeyState(ButtonsProtocol, &PowerKeyState);  // Change the data type to BUTTON_STATE*.
 
         if (EFI_ERROR(Status)) {
-            DEBUG((EFI_D_ERROR, "Error al leer el estado del botón de encendido: %r\n", Status));
+            DEBUG((EFI_D_ERROR, "Error reading the power key state: %r\n", Status));
             return Status;
         }
 
-        // Esperar un breve período antes de verificar nuevamente (evitar detección excesiva).
-        gBS->Stall(100000);  // Por ejemplo, espera 100,000 microsegundos (0.1 segundos).
+        // Wait for a brief period before checking again (avoid excessive detection).
+        gBS->Stall(100000);  // For example, wait for 100,000 microseconds (0.1 seconds).
 
     } while (PowerKeyState != BUTTON_PRESSED);
 
-    // Botón de encendido presionado, puedes realizar acciones adicionales aquí si es necesario.
+    // Power key pressed, you can perform additional actions here if necessary.
 
     return EFI_SUCCESS;
 }
 
+EFI_STATUS EFIAPI InitializeButtonsProtocol(IN EXYNOS_BUTTONS_PROTOCOL *This) {
+  // Perform button hardware initialization here if needed.
+  // Configure GPIO pins, resistors, interrupts, etc.
+  // This function is called when the button driver is loaded.
+
+  // For example, configure GPIO pins for the "Volume +" and "Volume -" buttons.
+  // This may include setting input directions and pull-up/down resistors.
+
+  // If no additional initialization is required, simply return EFI_SUCCESS.
+  return EFI_SUCCESS;
+}
+
 EFI_STATUS EFIAPI ButtonsInit(IN EFI_HANDLE ImageHandle, IN EFI_SYSTEM_TABLE *SystemTable) {
     EFI_STATUS Status;
-    EXYNOS_BUTTONS_PROTOCOL ButtonsProtocolInstance;  // Crea una instancia de la estructura.
+    EXYNOS_BUTTONS_PROTOCOL ButtonsProtocolInstance;  // Create an instance of the structure.
 
-    DEBUG((EFI_D_INFO, "Driver de botones GPIO inicializado.\n"));
+    DEBUG((EFI_D_INFO, "GPIO button driver initialized.\n"));
 
-    // Inicializa la instancia de EXYNOS_BUTTONS_PROTOCOL según tus necesidades.
-    // Por ejemplo, puedes inicializar campos específicos de la estructura aquí.
+    // Initialize the instance of EXYNOS_BUTTONS_PROTOCOL according to your needs.
+    // For example, you can initialize specific fields of the structure here.
 
-    // Configura los pines GPIO para los botones "Volumen +" y "Volumen -".
-    Status = InitializeButtonsProtocol(&ButtonsProtocolInstance);
+    // Configure GPIO pins for "Volume +" and "Volume -" buttons.
+    // Configure GPIO pins for wakeup functionality (if supported).
+
+    // Assuming you have a GPIO protocol to work with GPIO pins, you can use it like this:
+
+    // Locate the GPIO protocol.
+    EFI_GPIO_PROTOCOL *GpioProtocol;
+    Status = gBS->LocateProtocol(
+        &gEfiGpioProtocolGuid,  // Replace with the GUID of your GPIO protocol.
+        NULL,
+        (VOID**)&GpioProtocol
+    );
+
     if (EFI_ERROR(Status)) {
-        DEBUG((EFI_D_ERROR, "Error al inicializar los botones GPIO: %r\n", Status));
+        DEBUG((EFI_D_ERROR, "Failed to locate the GPIO protocol: %r\n", Status));
         return Status;
     }
 
-    // Resto de tu lógica de manejo de eventos de botones aquí.
+    // Configure the GPIO pins for wakeup functionality.
+    // The specific GPIO pin numbers and configurations depend on your hardware.
+    // Below is an example of configuring GPIO pins for wakeup.
+    UINT32 GpioPinList[] = {VOLUME_UP_GPIO_PIN, VOLUME_DOWN_GPIO_PIN};
+
+    for (UINTN i = 0; i < sizeof(GpioPinList) / sizeof(GpioPinList[0]); i++) {
+        Status = GpioProtocol->SetIoMode(
+            GpioProtocol,
+            GpioPinList[i],
+            EfiGpioIoInput,
+            EfiGpioDriveStrength6mA,
+            EfiGpioPullUp
+        );
+
+        if (EFI_ERROR(Status)) {
+            DEBUG((EFI_D_ERROR, "Failed to configure GPIO pin %d: %r\n", GpioPinList[i], Status));
+            return Status;
+        }
+
+        // Enable GPIO pin wakeup (if supported by hardware).
+        Status = GpioProtocol->SetWakeupEnable(GpioProtocol, GpioPinList[i], TRUE);
+
+        if (EFI_ERROR(Status)) {
+            DEBUG((EFI_D_ERROR, "Failed to enable GPIO pin %d wakeup: %r\n", GpioPinList[i], Status));
+            return Status;
+        }
+    }
+
+    // Rest of your button event handling logic here.
 
     return EFI_SUCCESS;
 }
---
+++ EXYNOS7885Pkg/Include/Protocol/Gpio.h
@@ -0,0 +1,94 @@
+#ifndef _GPIO_PROTOCOL_H_
+#define _GPIO_PROTOCOL_H_
+
+#include <Uefi/UefiBaseType.h>
+
+// Forward declaration of the protocol structure.
+typedef struct _EFI_GPIO_PROTOCOL EFI_GPIO_PROTOCOL;
+
+// Define GPIO pin configurations.
+typedef enum {
+  EfiGpioIoInput,
+  EfiGpioIoOutput,
+  // Add more configurations as needed.
+} EFI_GPIO_IO_MODE;
+
+typedef enum {
+  EfiGpioDriveStrength2mA,
+  EfiGpioDriveStrength4mA,
+  EfiGpioDriveStrength6mA,
+  // Add more drive strength levels as needed.
+} EFI_GPIO_DRIVE_STRENGTH;
+
+typedef enum {
+  EfiGpioPullDefault,
+  EfiGpioPullUp,
+  EfiGpioPullDown,
+  // Add more pull-up/pull-down configurations as needed.
+} EFI_GPIO_PULL_CONFIG;
+
+// Function prototypes for GPIO protocol functions.
+
+/**
+  Set the I/O mode of a GPIO pin.
+
+  @param[in]  This         A pointer to the EFI_GPIO_PROTOCOL instance.
+  @param[in]  GpioPin      The GPIO pin number to configure.
+  @param[in]  IoMode       The I/O mode to set for the GPIO pin.
+  @param[in]  DriveStrength The drive strength configuration for the GPIO pin.
+  @param[in]  PullConfig   The pull-up/pull-down configuration for the GPIO pin.
+
+  @retval EFI_SUCCESS      The GPIO pin was configured successfully.
+  @retval EFI_UNSUPPORTED  The GPIO pin configuration is not supported.
+  @retval Others           An error occurred while configuring the GPIO pin.
+**/
+typedef
+EFI_STATUS
+(EFIAPI *EFI_GPIO_SET_IO_MODE)(
+  IN EFI_GPIO_PROTOCOL *This,
+  IN UINT32 GpioPin,
+  IN EFI_GPIO_IO_MODE IoMode,
+  IN EFI_GPIO_DRIVE_STRENGTH DriveStrength,
+  IN EFI_GPIO_PULL_CONFIG PullConfig
+);
+
+/**
+  Enable or disable GPIO pin wakeup functionality (if supported).
+
+  @param[in]  This         A pointer to the EFI_GPIO_PROTOCOL instance.
+  @param[in]  GpioPin      The GPIO pin number to enable or disable for wakeup.
+  @param[in]  Enable       TRUE to enable wakeup, FALSE to disable it.
+
+  @retval EFI_SUCCESS      The GPIO pin wakeup was configured successfully.
+  @retval EFI_UNSUPPORTED  GPIO pin wakeup is not supported.
+  @retval Others           An error occurred while configuring GPIO pin wakeup.
+**/
+typedef
+EFI_STATUS
+(EFIAPI *EFI_GPIO_SET_WAKEUP_ENABLE)(
+  IN EFI_GPIO_PROTOCOL *This,
+  IN UINT32 GpioPin,
+  IN BOOLEAN Enable
+);
+
+/**
+  Other GPIO-related functions can be added here as needed.
+
+  For example, functions to read/write GPIO pin values, etc.
+
+**/
+
+/**
+  The EFI_GPIO_PROTOCOL structure.
+
+  This structure defines the GPIO protocol interface.
+**/
+struct _EFI_GPIO_PROTOCOL {
+  EFI_GPIO_SET_IO_MODE SetIoMode;
+  EFI_GPIO_SET_WAKEUP_ENABLE SetWakeupEnable;
+  // Add other GPIO-related function pointers here.
+};
+
+extern EFI_GUID gEfiGpioProtocolGuid;
+
+#endif // _GPIO_PROTOCOL_H_
