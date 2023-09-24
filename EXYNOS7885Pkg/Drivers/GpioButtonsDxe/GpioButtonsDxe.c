84d6ec74f9c1fcd49e706d7907f59ac1337348ba
Vicente Cortes <vicenteicc2008@gmail.com>
2023-09-24 12:19:40 -0300

    try to resolve this

--- EXYNOS7885Pkg/Drivers/GpioButtonsDxe/GpioButtonsDxe.c
+++ EXYNOS7885Pkg/Drivers/GpioButtonsDxe/GpioButtonsDxe.c
@@ -28,7 +28,7 @@ EFI_STATUS WaitForPowerKey() {
 
     do {
         // Leer el estado del botón de encendido ("power key").
-        Status = ButtonsProtocol->ReadEnterKeyState(ButtonsProtocol, (UINT8 *)&PowerKeyState);
+        Status = ButtonsProtocol->ReadEnterKeyState(ButtonsProtocol, (UINT8 *PowerKeyState));
 
         if (EFI_ERROR(Status)) {
             DEBUG((EFI_D_ERROR, "Error al leer el estado del botón de encendido: %r\n", Status));
