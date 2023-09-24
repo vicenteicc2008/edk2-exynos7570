bfc941d54eb0f7f055da385bbd9b87f9b543adbd
Vicente Cortes <vicenteicc2008@gmail.com>
2023-09-24 10:09:04 -0300

    update GpioButtonsDxe driver

--- EXYNOS7885Pkg/Drivers/GpioButtonsDxe/GpioButtonsDxe.c
+++ EXYNOS7885Pkg/Drivers/GpioButtonsDxe/GpioButtonsDxe.c
@@ -1,76 +1,63 @@
 #include <Uefi.h>
 #include <Library/UefiLib.h>
-#include <Library/MemoryAllocationLib.h>
 #include <Library/UefiBootServicesTableLib.h>
 #include <Library/DebugLib.h>
-#include <Protocol/ExynosButtons.h>
+#include <Protocol/ExynosButtons.h>  // Supongamos que existe un protocolo personalizado para los botones.
 
-// Definición de los números de pines GPIO para los botones
 #define VOLUME_UP_GPIO_PIN 7
 #define VOLUME_DOWN_GPIO_PIN 3
 
-EFI_STATUS
-EFIAPI
-ButtonsInit(
-    IN EFI_HANDLE ImageHandle,
-    IN EFI_SYSTEM_TABLE *SystemTable
-) {
+EFI_STATUS WaitForPowerKey() {
     EFI_STATUS Status;
+    UINT8 PowerKeyState;
 
-    // Inicializa la biblioteca UEFI para permitir la impresión en pantalla.
-    Status = UefiLibInitialize(ImageHandle, SystemTable);
-    if (EFI_ERROR(Status)) {
-        return Status;
-    }
+    // Supongamos que tienes un protocolo personalizado ExynosButtonsProtocol
+    // que proporciona acceso a los botones físicos, incluido el botón de encendido ("power key").
+    ExynosButtonsProtocol *ButtonsProtocol;
 
-    DEBUG((EFI_D_INFO, "Driver de botones GPIO inicializado.\n"));
-
-    // Configura los pines GPIO para los botones "Volumen +" y "Volumen -".
-    // Esto podría incluir la configuración de direcciones de entrada y resistencias pull-up/down.
-
-    // Registra el protocolo de botones GPIO en la tabla de protocolos.
-    // Reemplaza "YourGPIOProtocol" con el protocolo GPIO específico de tu plataforma.
-    Status = gBS->InstallMultipleProtocolInterfaces(
-        &ImageHandle,
-        &gExynosGpioProtocolGuid, // Reemplaza con la GUID de tu protocolo GPIO
-        &ExynosGpioProtocol,      // Reemplaza con una instancia válida del protocolo GPIO
-        NULL
+    Status = gBS->LocateProtocol(
+        &gExynosButtonsProtocolGuid,  // Reemplaza con la GUID de tu protocolo de botones.
+        NULL,
+        (VOID**)&ButtonsProtocol
     );
 
     if (EFI_ERROR(Status)) {
-        DEBUG((EFI_D_ERROR, "Error al registrar el protocolo de botones GPIO: %r\n", Status));
+        DEBUG((EFI_D_ERROR, "No se pudo encontrar el protocolo de botones: %r\n", Status));
         return Status;
     }
 
-    // Continúa con la lógica de manejo de eventos de botones GPIO aquí.
-    // Debes configurar la detección de eventos en los pines GPIO específicos
-    // para los botones "Volumen +" y "Volumen -", y realizar acciones en consecuencia.
+    do {
+        // Leer el estado del botón de encendido ("power key").
+        Status = ButtonsProtocol->ReadPowerKeyState(ButtonsProtocol, &PowerKeyState);
 
-    while (1) {
-        // Leer el estado de los pines GPIO de los botones "Volumen +" y "Volumen -".
-        BOOLEAN isVolumeUpPressed = ReadGPIO(VOLUME_UP_GPIO_PIN);
-        BOOLEAN isVolumeDownPressed = ReadGPIO(VOLUME_DOWN_GPIO_PIN);
-        
-        // Detección de eventos de botones
-        if (isVolumeUpPressed) {
-            // Botón "Volumen +" presionado
-            // Realiza acciones correspondientes, como aumentar el volumen
-            // o ejecutar una función específica.
-            DEBUG((EFI_D_INFO, "Botón 'Volumen +' presionado.\n"));
-            // Agrega aquí las acciones que deseas realizar para este botón.
+        if (EFI_ERROR(Status)) {
+            DEBUG((EFI_D_ERROR, "Error al leer el estado del botón de encendido: %r\n", Status));
+            return Status;
         }
-        
-        if (isVolumeDownPressed) {
-            // Botón "Volumen -" presionado
-            // Realiza acciones correspondientes, como disminuir el volumen
-            // o ejecutar una función específica.
-            DEBUG((EFI_D_INFO, "Botón 'Volumen -' presionado.\n"));
-            // Agrega aquí las acciones que deseas realizar para este botón.
-        }
-        
-        // Espera un breve período de tiempo para evitar la detección excesiva de eventos.
-        gBS->Stall(100000); // Por ejemplo, espera 100,000 microsegundos (0.1 segundos).
-    }
+
+        // Esperar un breve período antes de verificar nuevamente (evitar detección excesiva).
+        gBS->Stall(100000);  // Por ejemplo, espera 100,000 microsegundos (0.1 segundos).
+
+    } while (PowerKeyState != BUTTON_PRESSED);
+
+    // Botón de encendido presionado, puedes realizar acciones adicionales aquí si es necesario.
 
     return EFI_SUCCESS;
+}
+
+EFI_STATUS EFIAPI ButtonsInit(IN EFI_HANDLE ImageHandle, IN EFI_SYSTEM_TABLE *SystemTable) {
+  EFI_STATUS Status;
+  
+  DEBUG((EFI_D_INFO, "Driver de botones GPIO inicializado.\n"));
+
+  // Configura los pines GPIO para los botones "Volumen +" y "Volumen -".
+  Status = InitializeButtonsProtocol(&ExynosGpioProtocol);
+  if (EFI_ERROR(Status)) {
+    DEBUG((EFI_D_ERROR, "Error al inicializar los botones GPIO: %r\n", Status));
+    return Status;
+  }
+
+  // Resto de tu lógica de manejo de eventos de botones aquí.
+
+  return EFI_SUCCESS;
 }
--- EXYNOS7885Pkg/Include/Protocol/ExynosButtons.h
+++ EXYNOS7885Pkg/Include/Protocol/ExynosButtons.h
@@ -2,37 +2,34 @@
 #define _EXYNOS_BUTTONS_H_
 
 #include <Uefi.h>
+#include <Library/UefiLib.h>
 
-// Reemplaza con la GUID correcta para tu protocolo GPIO
-extern EFI_GUID gExynosGpioProtocolGuid;
+#define EXYNOS_BUTTONS_PROTOCOL_GUID \
+  { 0x1ebc06b9, 0x36e6, 0x452d, { 0x94, 0xb7, 0x1e, 0xc7, 0xf1, 0xe0, 0x55, 0x97 } }
 
-// Define una estructura para el protocolo GPIO de botones
-typedef struct _EXYNOS_BUTTONS_PROTOCOL EXYNOS_BUTTONS_PROTOCOL;
-
-// Enumeración para los posibles estados de los botones
 typedef enum {
-    ButtonStateUp,
-    ButtonStateDown,
-    // Agrega otros estados según sea necesario
+  BUTTON_NOT_PRESSED = 0,
+  BUTTON_PRESSED
 } BUTTON_STATE;
 
-// Función para inicializar el protocolo GPIO de botones
-EFI_STATUS
-EFIAPI
-InitializeExynosButtonsProtocol(
-    IN EFI_HANDLE ImageHandle,
-    IN EFI_SYSTEM_TABLE *SystemTable
+typedef struct _EXYNOS_BUTTONS_PROTOCOL EXYNOS_BUTTONS_PROTOCOL;
+
+typedef EFI_STATUS (EFIAPI *READ_ENTER_KEY_STATE)(
+  IN EXYNOS_BUTTONS_PROTOCOL *This,
+  OUT BUTTON_STATE *State
 );
 
-// Función para obtener el estado actual de un botón específico
-EFI_STATUS
-EFIAPI
-GetButtonState(
-    IN EXYNOS_BUTTONS_PROTOCOL *This,
-    IN UINTN ButtonIndex,
-    OUT BUTTON_STATE *State
+typedef EFI_STATUS (EFIAPI *INITIALIZE_BUTTONS_PROTOCOL)(
+  IN EXYNOS_BUTTONS_PROTOCOL *This
 );
 
-// Otras funciones y definiciones relacionadas con los botones GPIO
+struct _EXYNOS_BUTTONS_PROTOCOL {
+  READ_ENTER_KEY_STATE ReadEnterKeyState;      // Función para leer el estado del botón de encendido como "Enter"
+  INITIALIZE_BUTTONS_PROTOCOL InitializeButtons;
+};
+
+extern EFI_GUID gExynosButtonsProtocolGuid;
+
+EFI_STATUS EFIAPI InitializeButtonsProtocol(IN EXYNOS_BUTTONS_PROTOCOL *This);
 
 #endif // _EXYNOS_BUTTONS_H_
