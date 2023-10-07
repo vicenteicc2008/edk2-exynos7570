2eac88e3c80383bcf811ecd3175eb713e5ebbfb0
Vicente Cortes <vicenteicc2008@gmail.com>
2023-10-07 12:50:22 -0300

    add code and guid

--- EXYNOS7885Pkg/Drivers/ButtonsDxe/ButtonsDxe.c
+++ EXYNOS7885Pkg/Drivers/ButtonsDxe/ButtonsDxe.c
@@ -13,6 +13,9 @@
 #include <Protocol/SimpleTextIn.h>
 #include <Protocol/SimpleTextInEx.h>
 
+#define EXYNOS_KEYPAD_DEVICE_GUID \
+    { 0xD7F58A0E, 0xBED2, 0x4B5A, { 0xBB, 0x43, 0x8A, 0xB2, 0x3D, 0xD0, 0xE2, 0xB0} }
+
 // key buffer for passing key to console.
 #define MAX_KEYS_TO_BUFFER   32
 #define KEYPAD_TIMER_INTERVAL 500000  //50 milli seconds
@@ -58,6 +61,81 @@ KEY_TYPE  keyMap[MAX_KEYS_PRESSED_RELEASED];
 
 // To store the NotifyEntry of each notification.
 LIST_ENTRY     *NotifyList;
+
+typedef struct _KEYPAD_CONSOLE_IN_EX_NOTIFY {
+  EFI_HANDLE                            NotifyHandle;
+  EFI_KEY_DATA                          KeyData;
+  EFI_KEY_NOTIFY_FUNCTION               KeyNotificationFn;
+  LIST_ENTRY                            NotifyEntry;
+} KEYPAD_CONSOLE_IN_EX_NOTIFY;
+
+/* call back periodic timer maintained by the driver
+   in order to poll PMIC and inform the clients registered
+   with keypad driver about the status of input key. */
+EFI_EVENT      CallbackTimer;
+
+// EFI_SIMPLE_TEXT_INPUT_PROTOCOL implementation
+EFI_STATUS KeysDxeReset(
+  IN EFI_SIMPLE_TEXT_INPUT_PROTOCOL *This,
+  IN BOOLEAN                        ExtendedVerification );
+
+EFI_STATUS KeysDxeReadKeyStroke(
+  IN EFI_SIMPLE_TEXT_INPUT_PROTOCOL   *This,
+  OUT EFI_INPUT_KEY                   *Key);
+
+EFI_SIMPLE_TEXT_INPUT_PROTOCOL KeypadSimpleInputProtocolImplementation = {
+   KeysDxeReset,
+   KeysDxeReadKeyStroke,
+   0    //WaitForKey
+};
+
+/* Protos for EfiSimpleTextInputExProtocol implementation */
+EFI_STATUS
+EFIAPI
+KeysDxeResetEx (
+  IN EFI_SIMPLE_TEXT_INPUT_EX_PROTOCOL  *This,
+  IN BOOLEAN                            ExtendedVerification);
+
+EFI_STATUS
+EFIAPI
+KeysDxeReadKeyStrokeEx (
+  IN  EFI_SIMPLE_TEXT_INPUT_EX_PROTOCOL *This,
+  OUT EFI_KEY_DATA                      *KeyData);
+
+EFI_STATUS
+EFIAPI
+KeysDxeSetStateEx (
+  IN EFI_SIMPLE_TEXT_INPUT_EX_PROTOCOL  *This,
+  IN EFI_KEY_TOGGLE_STATE               *KeyToggleState);
+
+EFI_STATUS
+EFIAPI
+KeysDxeRegisterKeyNotify (
+  IN  EFI_SIMPLE_TEXT_INPUT_EX_PROTOCOL  *This,
+  IN  EFI_KEY_DATA                       *KeyData,
+  IN  EFI_KEY_NOTIFY_FUNCTION            KeyNotificationFunction,
+  OUT EFI_HANDLE                         *NotifyHandle);
+
+EFI_STATUS
+EFIAPI
+KeysDxeUnregisterKeyNotify (
+  IN EFI_SIMPLE_TEXT_INPUT_EX_PROTOCOL  *This,
+  IN EFI_HANDLE                         NotificationHandle);
+
+BOOLEAN
+EFIAPI
+IsKeyRegistered (
+  IN EFI_KEY_DATA  *RegisteredData,
+  IN EFI_KEY_DATA  *InputData );
+
+EFI_SIMPLE_TEXT_INPUT_EX_PROTOCOL KeypadSimpleInputExProtocolImplementation = {
+   KeysDxeResetEx,
+   KeysDxeReadKeyStrokeEx,
+   0,                          //  WaitForKeyEx
+   KeysDxeSetStateEx,
+   KeysDxeRegisterKeyNotify,
+   KeysDxeUnregisterKeyNotify,
+};
 
 BOOLEAN StoreAKey(
   KEY_TYPE *pArray,
