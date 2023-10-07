9b45beee7e9cd5bf8733687f659bf1bf33450b97
Vicente Cortes <vicenteicc2008@gmail.com>
2023-10-07 10:49:02 -0300

    clean up

--- EXYNOS7885Pkg/Drivers/ButtonsDxe/ButtonsDxe.c
+++ EXYNOS7885Pkg/Drivers/ButtonsDxe/ButtonsDxe.c
@@ -1,31 +1,35 @@
 // Copyright (C) 2023, vicenteicc2008. All rights reserved.<BR>
 
 #include <Uefi.h>
-#include <Library/PcdLib.h>
 #include <Library/UefiLib.h>
 #include <Library/DebugLib.h>
 #include <Library/UefiDriverEntryPoint.h>
 #include <Library/UefiRuntimeServicesTableLib.h>
-#include <Library/UefiBootServicesTableLib.h>
-#include <Library/BaseMemoryLib.h>
-#include <Library/MemoryAllocationLib.h>
-#include <Library/DevicePathLib.h>
 
-EFI_STATUS ButtonsInitialize (
-  IN EFI_HANDLE         ImageHandle,
-  IN EFI_SYSTEM_TABLE   *SystemTable
+EFI_STATUS EFIAPI ButtonsInitialize(
+  IN EFI_HANDLE ImageHandle,
+  IN EFI_SYSTEM_TABLE *SystemTable
+) {
+  EFI_STATUS Status = EFI_SUCCESS;
+
+  DEBUG((EFI_D_INFO, "Hello from ButtonsDxe\n"));
+
+  // Tu código de inicialización de botones va aquí
+  // ...
+
+  if (EFI_ERROR(Status)) {
+    DEBUG((EFI_D_ERROR, "ButtonsDxeInitialize: Failed, Status = (0x%x)\n", Status));
+  }
+
+  return Status;
+}
+
+EFI_STATUS
+EFIAPI
+UefiMain (
+  IN EFI_HANDLE        ImageHandle,
+  IN EFI_SYSTEM_TABLE  *SystemTable
 )
 {
-	EFI_STATUS  Status;
-
-    DEBUG((EFI_D_INFO, "Hello from ButtonsDxe"));
-
-	if (EFI_ERROR (Status))
-    {
-      DEBUG(( EFI_D_INFO, "ButtonsDxeInitialize: ButtonsDxeInitialize failed, Status = (0x%x)\r\n", Status));
-      goto ErrorExit;
-    }
-
-	ErrorExit:
-      return Status;
+  return ButtonsInitialize(ImageHandle, SystemTable);
 }
