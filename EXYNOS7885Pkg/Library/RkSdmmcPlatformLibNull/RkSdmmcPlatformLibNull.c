dbab5234d3bb114e79da4b2449cc91d02053cf97
Vicente Cortes <vicenteicc2008@gmail.com>
2023-11-28 15:08:42 -0300

    add some eMMC drivers

---
+++ EXYNOS7885Pkg/Library/RkSdmmcPlatformLibNull/RkSdmmcPlatformLibNull.c
@@ -0,0 +1,39 @@
+/** @file
+ *
+ *  RkSdmmcDxe platform helper library.
+ *
+ *  Copyright (c) 2023, Mario Bălănică <mariobalanica02@gmail.com>
+ *
+ *  SPDX-License-Identifier: BSD-2-Clause-Patent
+ *
+ **/
+
+#include <Uefi.h>
+#include <Library/RkSdmmcPlatformLib.h>
+
+EFI_STATUS
+EFIAPI
+RkSdmmcSetClockRate (
+  IN UINTN Frequency
+  )
+{
+  return EFI_UNSUPPORTED;
+}
+
+VOID
+EFIAPI
+RkSdmmcSetIoMux (
+  VOID
+  )
+{
+  return;
+}
+
+RKSDMMC_CARD_PRESENCE_STATE
+EFIAPI
+RkSdmmcGetCardPresenceState (
+  VOID
+  )
+{
+  return RkSdmmcCardPresenceUnsupported;
+}
